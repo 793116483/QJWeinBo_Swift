86d3496462fdba6cb7a05a18b964e71474178d9a
瞿杰 <793116483@qq.com>
2019-11-07 13:32:08 +0800

    通过控制器名称创建控制对象，先拿到命名空间才能创建到对象

--- QJWeiBo/Classes/Main(主要)/QJTabBarViewController.swift
+++ QJWeiBo/Classes/Main(主要)/QJTabBarViewController.swift
@@ -26,16 +26,33 @@ private extension QJTabBarViewController {
         
         // Do any additional setup after loading the view.
         // 添加首页
-        self.addChildViewController(vc: QJHomeViewController(), title: "首页", imageName: "tabbar_home")
+        self.addChildViewController(vcName: "QJHomeViewController", title: "首页", imageName: "tabbar_home")
         // 添加消息
-        self.addChildViewController(vc: QJMessageViewController(), title: "消息", imageName: "tabbar_message_center")
+        self.addChildViewController(vcName: "QJMessageViewController", title: "消息", imageName: "tabbar_message_center")
         // 添加发现
-        self.addChildViewController(vc: QJDiscoverViewController(), title: "发现", imageName: "tabbar_discover")
+        self.addChildViewController(vcName: "QJDiscoverViewController", title: "发现", imageName: "tabbar_discover")
         // 添加我的
-        self.addChildViewController(vc: QJProfileViewController(), title: "我", imageName: "tabbar_profile")
+        self.addChildViewController(vcName: "QJProfileViewController", title: "我", imageName: "tabbar_profile")
     }
     // 添加子控制器
-     func addChildViewController(vc:UIViewController , title:String , imageName:String) {
+     func addChildViewController(vcName:String , title:String , imageName:String) {
+        
+        // 1.获取命名空间,从info.plist中取命名空间就是项目名QJWeiBo
+        guard let nameSpace = Bundle.main.infoDictionary![kCFBundleExecutableKey as String] as? String else {
+            print("未找到命名空间")
+            return
+        }
+        // 2.根据字符串获取class
+        guard let vcClass = NSClassFromString(nameSpace + "." + vcName) else {
+            print("未找到对应的class")
+            return
+        }
+        // 3.对AnyClass转成控制器类型
+        guard let vcType = vcClass as? UIViewController.Type else {
+            return
+        }
+        let vc = vcType.init()
+        
         vc.title = title
         vc.tabBarItem.image = UIImage(named: imageName)
         vc.tabBarItem.selectedImage = UIImage(named: imageName+"_highlighted")
